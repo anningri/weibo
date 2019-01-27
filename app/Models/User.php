5f0d09e8ce125a13bdc5005baa180ccd1304e868
anningri <minganwujian@qq.com>
2019-01-27 05:51:25 +0000

    用户显示界面

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,15 @@ class User extends Authenticatable
     ];
 
     protected $table = "users";
+
+
+    /**
+     * get gravatar.com avatar
+     *
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
