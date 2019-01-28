96e137be99b1f57ffce6d39afb82349454a41190
anningri <minganwujian@qq.com>
2019-01-28 00:04:21 +0000

    incorrect config for pgsql in config/database.php version 2 and error in line 66

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             'driver' => 'pgsql',
             'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => $db_config['dtabase'],
+            'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
             'charset' => 'utf8',
