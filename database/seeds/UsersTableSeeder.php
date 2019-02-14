12f7433854bdff39605746f7a9b3b290a09d3277
anningri <minganwujian@qq.com>
2019-02-14 07:38:52 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Leman';
         $user->email = 'leman.li@otmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
