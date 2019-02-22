97ed51c8616278d0b8e5aa089fcf20c44bd2dcac
anningri <minganwujian@qq.com>
2019-02-22 08:08:03 +0000

    用户激活

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,14 @@ class User extends Authenticatable
 
     protected $table = "users";
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
 
     /**
      * get gravatar.com avatar
